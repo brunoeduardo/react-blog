40e01e6a5823b3800536123202ca6324d043d1c2
Bruno Eduardo <bruno@beduardo.com>
2024-07-02 18:22:11 -0300

    add banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,25 @@
+import "./Banner.css"
+import colorfulCircle from "assets/colorful_circle.png"
+import myPhoto from "assets/about_me.png"
+
+
+const Banner = () => {
+    return(
+        <div className="banner">
+            <div>
+                <h1 className="title">
+                    Hello world!
+                </h1>
+                <p className="paragraph">
+                    Welcome to my personal space! I am Antônio Evaldo, a Front-end instructor at Alura. Here I share various knowledge, and I hope you learn something new :)
+                </p>
+            </div>
+            <div className="images">
+                <img className="colorfulCircle" src={colorfulCircle} alt="" aria-hidden={true}/>
+                <img className="myPhoto" src={myPhoto} alt="Perfil photography" />
+            </div>
+        </div>
+    )
+}
+
+export default Banner
